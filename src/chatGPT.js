49f8b0decad9e2984eb21c5a3fbe86b9a3e7536c
chengxili <chengxilii@outlook.com>
2023-09-30 11:03:18 -0400

    Implemented function that using chatGPT API

--- src/chatGPT.js
+++ src/chatGPT.js
@@ -1,102 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect }  from 'react';
-import './App.css';
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-  TypingIndicator,
-} from '@chatscope/chat-ui-kit-react';
+import OpenAI from 'openai';
 
-const API_KEY =""
+const openai = new OpenAI({
+  apiKey: 'my api key', 
+});
 
-const App = () => {
-  const [messages, setMessages] = useState([
-    {
-      message: "Hello, I'm ChatGPT! Ask me anything!",
-      sentTime: "just now",
-      sender: "ChatGPT",
-    },
-  ]);
-  const [isTyping, setIsTyping] = useState(false);
+async function checkPlantInvasiveness(plant, region) {
+  const result = [];
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: `Is "${plant}" invasive in "${region}", please respond with 'Yes' or 'No'.`}],
+    model: 'gpt-4',
+  });
 
-  const handleSendRequest = async (message) => {
-    const newMessage = {
-      message,
-      direction: 'outgoing',
-      sender: "user",
-    };
+  result.push(chatCompletion.choices[0][text])
 
-    setMessages((prevMessages) => [...prevMessages, newMessage]);
-    setIsTyping(true);
+  await openai.chat.completions.create({
+    messages: [{ role: 'user', content: `Can you give me some information about "${plant}"?`}],
+    model: 'gpt-4',
+  });
+  result.push(chatCompletion.choices[1][text])
 
-    try {
-      const response = await processMessageToChatGPT([...messages, newMessage]);
-      const content = response.choices[0]?.message?.content;
-      if (content) {
-        const chatGPTResponse = {
-          message: content,
-          sender: "ChatGPT",
-        };
-        setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
-      }
-    } catch (error) {
-      console.error("Error processing message:", error);
-    } finally {
-      setIsTyping(false);
-    }
-  };
-
-  async function processMessageToChatGPT(chatMessages) {
-    const apiMessages = chatMessages.map((messageObject) => {
-      const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
-      return { role, content: messageObject.message };
-    });
-
-    const apiRequestBody = {
-      "model": "gpt-4",
-      "messages": [
-        { role: "system", content: "I'm a Student using ChatGPT for learning" },
-        ...apiMessages,
-      ],
-    };
-
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(apiRequestBody),
-    });
-
-    return response.json();
-  }
-
-  return (
-    <div className="App">
-      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
-        <MainContainer>
-          <ChatContainer>       
-            <MessageList 
-              scrollBehavior="smooth" 
-              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
-            >
-              {messages.map((message, i) => {
-                console.log(message)
-                return <Message key={i} model={message} />
-              })}
-            </MessageList>
-            <MessageInput placeholder="Send a Message" onSend={handleSendRequest} />        
-          </ChatContainer>
-        </MainContainer>
-      </div>
-    </div>
-  )
+  return result
 }
-
-export default App;
